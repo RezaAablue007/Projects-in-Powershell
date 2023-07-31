37d50ed2520d2fc6259dbe0a1f85f059ac7d0bd9
Reza Aablue <77694155+RezaAablue007@users.noreply.github.com>
2023-07-31 16:59:53 -0400

    Add files via upload

---
+++ S0i3Settings/TestSigning.cmd
@@ -0,0 +1,3 @@
+bcdedit /set testsigning on
+wmic recoveros set AutoReboot = False
+wmic recoveros set DebugInfoType = 1
---
+++ S0i3Settings/TestSigning.ps1
@@ -0,0 +1,3 @@
+. C:\Windows\System32\.\ace_spy.ps1
+
+\\90tctac01\Tests$\td176\2782104\TestSigning.cmd
