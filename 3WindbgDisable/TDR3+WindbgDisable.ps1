398f5fb6602c12c660e02c9c24f05de17d1a5165
Reza Aablue <77694155+RezaAablue007@users.noreply.github.com>
2023-07-31 16:58:14 -0400

    Add files via upload

---
+++ 3WindbgDisable/TDR3+WindbgDisable.ps1
@@ -0,0 +1,12 @@
+﻿# Author: Reza Aablue
+
+#Importing S0i3 Regkeys & Windows Maintenance Disable Regkey
+$PathofRegFile="C:\TestRes\Ace_H_Set_TDR3_DisableWindbg\TDR3.reg"
+regedit /s $PathofRegFile
+
+#Disable WinDBG
+bcdedit /debug off
+
+Get-WmiObject -Class Win32_OSRecoveryConfiguration -EnableAllPrivileges |
+Set-WmiInstance -Arguments @{ DebugInfoType=1 }
+wmic recoveros set AutoReboot = True
